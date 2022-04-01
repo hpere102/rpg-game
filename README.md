7041c44eb3dcf5ff2488d3e36b61094e12f89017
danny p <danny@dannys-MacBook-Pro.local>
2022-03-31 23:31:38 -0400

    player movement

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # RPG Game
 
+
 ## Game Description
 
 Pokemon-like topdown game in which character is spawned on an island and can fight battles.
 (In progress...)
+
+![Website layout](images/screenshot.png)
 
 ## Made With
 
--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
+
+
 const image = new Image()
 image.src = './images/new-map.png'
+console.log(image)
 
-const foregroundImage = new Image()
-foregroundImage.src = './img/foregroundObjects.png'
 
 const playerDownImage = new Image()
 playerDownImage.src = './images/down.png'
@@ -22,20 +23,65 @@ playerLeftImage.src = './images/left.png'
 const playerRightImage = new Image()
 playerRightImage.src = './images/right.png'
 
-const keys = {
-    w: {
-      toggle: false
-    },
-    a: {
-      toggle: false
-    },
-    s: {
-      toggle: false
-    },
-    d: {
-      toggle: false
-    }
+class Sprite {
+  constructor({ position, velocity, image }) {
+    this.position = position
+    this.image = image
+    
   }
+
+  draw() {
+    c.drawImage(this.image, this.position.x, this.position.y)
+  }
+}
+
+const background = new Sprite ({
+  position: {
+    x:0,
+    y:-2610
+  },
+  image: image
+})
+
+const keys = {
+  w: {
+    toggle: false
+  },
+  a: {
+    toggle: false
+  },
+  s: {
+    toggle: false
+  },
+  d: {
+    toggle: false
+  }
+}
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  background.draw()
+  c.drawImage(
+    playerDownImage,
+    0,
+    0,
+    playerDownImage.width / 4,
+    playerDownImage.height,
+    canvas.width / 2 - (playerDownImage.width / 4) / 2,
+    canvas.height / 2 - playerDownImage.height / 2,
+    playerDownImage.width / 4,
+    playerDownImage.height
+  )
+  if (keys.w.toggle) { background.position.y = background.position.y + 3
+  } else if (keys.a.toggle) { background.position.x = background.position.x + 3
+  } else if (keys.s.toggle) { background.position.y = background.position.y - 3
+  } else if (keys.d.toggle) { background.position.x = background.position.x - 3
+  }
+
+}
+
+animate();
+
 
 let newKey = "" 
 window.addEventListener('keydown', (e) => {
@@ -77,3 +123,4 @@ window.addEventListener('keyup', (e) => {
       break
   }
 })
+
